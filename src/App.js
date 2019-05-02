fc8e2519817b2ab4b2935f00d16d332abadb8225
PrashanthAdurugatla <padurugatla@horizon.csueastbay.edu>
2019-05-01 19:50:24 -0700

    Uses Heroku link to connect Backend

--- src/App.js
+++ src/App.js
@@ -8,33 +8,57 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import Clarifai from 'clarifai';
+
+
+const app = new Clarifai.App({
+    apiKey: '1d009cd362b1461a898add23c34e4420'
+});
 
 
 
 const particlesOptions = {
     particles: {
         number: {
-            value: 100,
+            value: 40,
             density: {
                 enable: true,
-                value_area: 600
+                value_area: 1500,
             }
         },
-        color: {
-            value: "#ffffff"
+        size: {
+            value: 0,
+            random: true,
         },
-        shape: {
-            type: "circle",
-            stroke: {
-                width: 1,
-                color: "#71a760"
-            },
-            polygon: {
-                "nb_sides": 3
+        opacity: {
+            value: 1,
+        },
+        line_linked: {
+            color: '#EE4B6A'
+        },
+        move: {
+            enable: true,
+            speed: 3,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
             }
+        }
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        onresize: {
+            density_auto: true
         }
     }
 }
+
 const initialState = {
     input: '',
     imageUrl: '',
@@ -88,39 +112,41 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
-        fetch('http://localhost:3000/imageurl', {
+        /*fetch('https://face-rec-server.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 input: this.state.input
             })
-        })
+        })*/
 
         /* Moved to backend. It Predicts the contents of an image by passing in a URL.
+        */
         app.models
             .predict(
                 Clarifai.FACE_DETECT_MODEL,
                 this.state.input)
-*/
-        .then(response => {
-            if (response) {
-                fetch('http://localhost:3000/image', {
-                    method: 'put',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({
-                        id: this.state.user.id
-                    })
-                })
-                .then(response => response.json())
-                .then(count => {
-                    this.setState(Object.assign(this.state.user, {entries: count}))
-                })
-                .catch(console.log);
 
-            }
-            this.displayFaceBox(this.calculateFaceLocation(response))
-        })
-        .catch(err => console.log(err));
+            .then(response => {
+                if (response) {
+                    fetch('https://face-rec-server.herokuapp.com/image', {
+                        method: 'put',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            id: this.state.user.id
+                        })
+                    })
+                        .then(response => response.json())
+
+                        .then(count => {
+                            this.setState(Object.assign(this.state.user, {entries: count}))
+                        })
+                        .catch(console.log);
+
+                }
+                this.displayFaceBox(this.calculateFaceLocation(response))
+            })
+            .catch(console.log);
     }
 
     onRouteChange = (route) => {
